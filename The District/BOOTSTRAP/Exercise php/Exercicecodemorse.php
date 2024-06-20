f225afc175908ef1144a390e993c965ebacb3ac0
Mumtaz-github <mumtazfra@gmail.com>
2024-06-20 15:22:22 +0200

    upde

--- The District/BOOTSTRAP/Exercise php/Exercicecodemorse.php
+++ The District/BOOTSTRAP/Exercise php/Exercicecodemorse.php
@@ -37,20 +37,34 @@ function stringToMorse($string) {
         '6' => '-....', 
         '7' => '--...',
         '8' => '---..',
-        '9' => '----.'  /* here when i put '' => '' or "" => "" for space it give me error syntax*/
-        );
-
+        '9' => '----.' 
+                         /* here when i put '' => '' or "" => "" for space it give me error syntax*/
+    );
     return strtr(strtoupper($string), $morseCode); //strtr — Translate characters or replace substrings and (strtoupper — Make a string uppercase
-}
-$string = 'bonjour de ma part'; //it translates this sentence to morseCode -...----..------..-.-. -... --.- .--..-.-.-
-echo stringToMorse($string); 
+    }
+    $string = 'bonjour de ma part'; //it translates this sentence to morseCode -...----..------..-.-. -... --.- .--..-.-.-
+    echo stringToMorse($string); 
+ ?>
+
+<!-- //this below n1 translate words in search bar
+/*1. if (isset($_POST['input'])) {
+     $input = $_POST['input'];
+     $morseCode = stringToMorse($input);
+     echo "<p>Morse code: $morseCode</p>";
+ }
+<form action="" method="post">
+    <input type="text" name="input" placeholder="Enter a word or phrase">
+   <button type="submit">Translate to Morse code</button>
+</form>*/
 
 
+//to add table or show result of each word in table we add the following code ..
+//2. $words = explode(' ', $string);
 
-
-
-
-
-
-?>
+// echo '<table border="1">';
+// foreach ($words as $word) {
+//     $morseWord = stringToMorse($word);
+//     echo '<tr><td>' . htmlspecialchars($word) . '</td><td>' . htmlspecialchars($morseWord) . '</td></tr>';
+// }
+// echo '</table>'; --> -->
 
